2f7b46f9bf9e924d261523011c3305ad14dd0aa9
Holden Whitehead <28017008+HW13@users.noreply.github.com>
2021-06-15 16:12:52 -0700

    chore: Improve examples

--- example/opentelemetry.js
+++ example/opentelemetry.js
@@ -1,18 +1,22 @@
-const { HttpTraceContextPropagator } = require('@opentelemetry/core')
-const { AsyncHooksContextManager } = require('@opentelemetry/context-async-hooks')
 const {
-  BasicTracerProvider,
   ConsoleSpanExporter,
   SimpleSpanProcessor
 } = require('@opentelemetry/tracing')
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http')
+const { NodeTracerProvider } = require('@opentelemetry/node')
+const { registerInstrumentations } = require('@opentelemetry/instrumentation')
 
-const provider = new BasicTracerProvider()
+const provider = new NodeTracerProvider()
 
 provider.addSpanProcessor(
+  // Note: SimpleSpanProcessor should not be used in production (use BatchSpanProcessor instead).
   new SimpleSpanProcessor(new ConsoleSpanExporter())
 )
 
-provider.register({
-  contextManager: (new AsyncHooksContextManager()).enable(),
-  propagator: new HttpTraceContextPropagator()
+provider.register()
+
+registerInstrumentations({
+  instrumentations: [
+    new HttpInstrumentation()
+  ]
 })
