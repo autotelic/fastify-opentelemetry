fc9ea145d4771f17fe254d8d9ca8b4ff6a1c34a1
Holden Whitehead <holden@autotelic.com>
2022-08-26 17:17:34 -0700

    chore: Update example app (#49)

--- example/index.js
+++ example/index.js
@@ -1,74 +1,77 @@
 // Load configured OpenTelemetry SDK first.
 require('./opentelemetry')
 
-const fastify = require('fastify')
+const fastify = require('fastify')()
 const fetch = require('node-fetch')
 const fastifyOpentelemetry = require('..')
 
-const boredApi = 'https://www.boredapi.com'
-const app = fastify()
+const boredApi = 'http://www.boredapi.com'
 
-app.register(fastifyOpentelemetry, { serviceName: 'basic-example', wrapRoutes: true })
+const start = async () => {
+  await fastify.register(fastifyOpentelemetry, { serviceName: 'basic-example', wrapRoutes: true })
 
-app.get('/', async function routeHandler (request, reply) {
-  const {
-    tracer
-  } = request.openTelemetry()
-  let childSpan
-  try {
-    // @opentelemetry/instrumentation-http will automatically trace incoming and out going http/https requests.
-    const activityRes = await fetch(`${boredApi}/api/activity`)
-    // Spans started in a wrapped route will automatically be children of the activeSpan.
-    childSpan = tracer.startSpan('preparing content')
-    const { activity } = await activityRes.json()
-    reply.type('text/html')
-    return `<h1>Bored?</h1><h3>Have you tried to ${activity.toLowerCase()}</h3>`
-  } catch (error) {
-    // fastify-opentelemetry automatically adds error data to the parent spans attributes.
-    return error
-  } finally {
-    // Always be sure to end child spans.
-    if (childSpan) childSpan.end()
-  }
-})
-
-app.get('/:type', async function routeHandler (request, reply) {
-  const { tracer } = request.openTelemetry()
-  let childSpan
-  try {
-    // @opentelemetry/instrumentation-http will automatically trace incoming and out going http/https requests.
-    const activityRes = await fetch(
-      `${boredApi}/api/activity?type=${request.params.type}`
-    )
-    // Spans started in a wrapped route will automatically be children of the activeSpan.
-    childSpan = tracer.startSpan(`preparing content of ${request.params.type}`)
-    const { activity, error } = await activityRes.json()
-
-    if (error) {
-      throw error
+  fastify.get('/', async function routeHandler (request, reply) {
+    const {
+      tracer
+    } = request.openTelemetry()
+    let childSpan
+    try {
+      // @opentelemetry/instrumentation-http will automatically trace incoming and out going http/https requests.
+      const activityRes = await fetch(`${boredApi}/api/activity`)
+      // Spans started in a wrapped route will automatically be children of the activeSpan.
+      childSpan = tracer.startSpan('preparing content')
+      const { activity } = await activityRes.json()
+      reply.type('text/html')
+      return `<h1>Bored?</h1><h3>Have you tried to ${activity.toLowerCase()}</h3>`
+    } catch (error) {
+      // fastify-opentelemetry automatically adds error data to the parent spans attributes.
+      return error
+    } finally {
+      // Always be sure to end child spans.
+      if (childSpan) childSpan.end()
     }
+  })
 
-    reply.type('text/html')
-    return `<h1>Bored?</h1><h3>Have you tried to ${activity.toLowerCase()}</h3>`
-  } catch (error) {
-    // fastify-opentelemetry automatically adds error data to the parent spans attributes.
-    return error
-  } finally {
-    // Always be sure to end child spans.
-    if (childSpan) childSpan.end()
-  }
-})
+  fastify.get('/:type', async function routeHandler (request, reply) {
+    const { tracer } = request.openTelemetry()
+    let childSpan
+    try {
+      // @opentelemetry/instrumentation-http will automatically trace incoming and out going http/https requests.
+      const activityRes = await fetch(
+        `${boredApi}/api/activity?type=${request.params.type}`
+      )
+      // Spans started in a wrapped route will automatically be children of the activeSpan.
+      childSpan = tracer.startSpan(`preparing content of ${request.params.type}`)
+      const { activity, error } = await activityRes.json()
 
-const PORT = process.env.PORT || 3000
+      if (error) {
+        throw error
+      }
 
-app.listen(PORT, (error) => {
-  if (error) {
-    console.error(error)
-    process.exit(1)
-  }
-  console.info(`
-  *** Listening on Port: ${PORT} ***
+      reply.type('text/html')
+      return `<h1>Bored?</h1><h3>Have you tried to ${activity.toLowerCase()}</h3>`
+    } catch (error) {
+      // fastify-opentelemetry automatically adds error data to the parent spans attributes.
+      return error
+    } finally {
+      // Always be sure to end child spans.
+      if (childSpan) childSpan.end()
+    }
+  })
 
-  * Navigate to http://localhost:${PORT}/ and then check back here to see the exported OpenTelemetry spans. *
-  `)
-})
+  const port = process.env.PORT || 3000
+
+  fastify.listen({ port }, (error) => {
+    if (error) {
+      console.error(error)
+      process.exit(1)
+    }
+    console.info(`
+    *** Listening on Port: ${port} ***
+
+    * Navigate to http://localhost:${port}/ and then check back here to see the exported OpenTelemetry spans. *
+    `)
+  })
+}
+
+start()
