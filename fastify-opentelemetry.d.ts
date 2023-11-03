cd94a935724b53183a1c41a281fdad2b653f6c3e
Holden Whitehead <28017008+HW13@users.noreply.github.com>
2023-11-03 13:52:17 -0700

    chore: fixfailing type tests

--- fastify-opentelemetry.d.ts
+++ fastify-opentelemetry.d.ts
@@ -1,38 +1,45 @@
 import { FastifyPluginCallback, FastifyReply, FastifyRequest } from 'fastify'
-import { Context, Span, SpanAttributes, TextMapGetter, TextMapSetter, Tracer } from '@opentelemetry/api'
+import { Context, Span, Attributes, TextMapGetter, TextMapSetter, Tracer } from '@opentelemetry/api'
 
 /**
  * Object exposed as part of the "openTelemetry" object on the fastify request.
  */
-export interface OpenTelemetryReqInstance {
-  readonly activeSpan: Span | undefined,
-  readonly context: Context,
-  readonly tracer: Tracer,
-  readonly inject: <Carrier> (carrier: Carrier, setter?: TextMapSetter) => void,
-  readonly extract: <Carrier> (carrier: Carrier, getter?: TextMapGetter) => Context,
+type ReqInstance = {
+  activeSpan: Span | undefined,
+  context: Context,
+  tracer: Tracer,
+  inject: <Carrier> (carrier: Carrier, setter?: TextMapSetter) => void,
+  extract: <Carrier> (carrier: Carrier, getter?: TextMapGetter) => Context,
 }
 
 declare module 'fastify' {
   interface FastifyRequest {
-    readonly openTelemetry: () => OpenTelemetryReqInstance,
+    openTelemetry: () => ReqInstance
   }
 }
 
 /**
  * Options for the OpenTelemetry plugin.
  */
-export interface OpenTelemetryPluginOptions {
-  readonly exposeApi?: boolean,
-  readonly formatSpanName?: (request: FastifyRequest) => string,
-  readonly formatSpanAttributes?: {
-    readonly request?: (request: FastifyRequest) => SpanAttributes,
-    readonly reply?: (reply: FastifyReply) => SpanAttributes,
-    readonly error?: (error: Error) => SpanAttributes,
-  },
-  readonly wrapRoutes?: boolean | string[],
-  readonly ignoreRoutes?: string[] | ((path: string, method: string) => boolean),
-}
 
-declare const fastifyOpenTelemetry: FastifyPluginCallback<OpenTelemetryPluginOptions>
+declare namespace fastifyOpenTelemetry {
+  export type OpenTelemetryReqInstance = ReqInstance
+
+  export type OpenTelemetryPluginOptions = {
+    exposeApi?: boolean,
+    formatSpanName?: (request: FastifyRequest) => string,
+    formatSpanAttributes?: {
+      request?: (request: FastifyRequest) => Attributes,
+      reply?: (reply: FastifyReply) => Attributes,
+      error?: (error: Error) => Attributes,
+    },
+    wrapRoutes?: boolean | string[],
+    ignoreRoutes?: string[] | ((path: string, method: string) => boolean),
+  }
+
+  export const fastifyOpenTelemetry: FastifyPluginCallback<OpenTelemetryPluginOptions>
+
+  export { fastifyOpenTelemetry as default }
+}
 
 export = fastifyOpenTelemetry
