accd4e93182ccd024263c18c6a0121ba409dc282
Holden Whitehead <holden@autotelic.com>
2024-02-09 16:15:50 -0800

    Update example for #63

--- example/opentelemetry.js
+++ example/opentelemetry.js
@@ -1,12 +1,30 @@
+const api = require('@opentelemetry/api')
 const {
   ConsoleSpanExporter,
-  SimpleSpanProcessor
+  SimpleSpanProcessor,
+  NoopSpanProcessor
 } = require('@opentelemetry/sdk-trace-base')
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http')
 const { registerInstrumentations } = require('@opentelemetry/instrumentation')
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node')
 
 const provider = new NodeTracerProvider()
+
+class ShareParentAttrsProcessor extends NoopSpanProcessor {
+  onStart (span, parentContext) {
+    // Check if the span was created by this plugin...
+    if (span.instrumentationLibrary.name === '@autotelic/fastify-opentelemetry') {
+      const parentSpan = api.trace.getSpan(parentContext)
+      if (parentSpan?.attributes) {
+        span.setAttributes(parentSpan.attributes)
+      }
+    }
+  }
+}
+
+provider.addSpanProcessor(
+  new ShareParentAttrsProcessor()
+)
 
 provider.addSpanProcessor(
   // Note: SimpleSpanProcessor should not be used in production (use BatchSpanProcessor instead).
